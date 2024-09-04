b35b42cb084307f161008692885cf8ec705819e1
Liu Jie <jieliu2000@live.com>
2024-09-04 18:20:45 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-use std::{
-    cell::{Ref, RefCell},
-    rc::Rc,
-};
+use std::{cell::RefCell, rc::Rc};
 
 use fltk::{
-    app,
-    button::{self, *},
-    draw::{self, height},
-    enums::{self, Event, FrameType},
+    app::{self, MouseButton, WidgetId},
+    button::{self},
+    draw::{self},
+    enums::{self, Event},
     frame,
     group::{self, Flex, Tabs},
     input,
-    prelude::{GroupExt, InputExt, MenuExt, WidgetBase, WidgetExt, WindowExt},
-    text, widget, widget_extends,
+    prelude::{GroupExt, InputExt, WidgetBase, WidgetExt, WindowExt},
+    widget::{self, Widget},
+    widget_extends,
     window::Window,
 };
+use fltk_accesskit::{AccessibilityContext, Accessible, AccessibleApp};
 use fltk_theme::{ThemeType, WidgetTheme};
 
 struct MouseTestArea {
@@ -36,8 +35,7 @@ impl IntesEvent {
 struct MouseKeyboardActionContext {
     mouse_x: i32,
     mouse_y: i32,
-    mouse_action: String,
-    mouse_button: String,
+    button: String,
 }
 impl MouseTestArea {
     // our constructor
@@ -45,10 +43,11 @@ impl MouseTestArea {
         label: &str,
         width: i32,
         height: i32,
-        mouse_action_handler: Rc<RefCell<MouseKeyboardActionContext>>,
+        mouse_action_context: Rc<RefCell<MouseKeyboardActionContext>>,
     ) -> Self {
         let mut inner = widget::Widget::default()
             .with_size(width - 40, height)
+            .with_id("mouse_test_canvas")
             .with_label(label);
 
         inner.set_frame(enums::FrameType::DownBox);
@@ -60,8 +59,23 @@ impl MouseTestArea {
             draw::set_font(enums::Font::Helvetica, app::font_size());
             draw::draw_text2(&i.label(), i.x(), i.y(), i.w(), i.h(), i.align());
         });
-        inner.handle(move |i, ev| match ev {
+        inner.handle(move |_i, ev| match ev {
             enums::Event::Push => {
+                let button: MouseButton = app::event_mouse_button();
+                match button {
+                    MouseButton::Left => {
+                        mouse_action_context.borrow_mut().button = "Left".to_string();
+                    }
+                    MouseButton::Middle => {
+                        mouse_action_context.borrow_mut().button = "Middle".to_string();
+                    }
+                    MouseButton::Right => {
+                        mouse_action_context.borrow_mut().button = "Right".to_string();
+                    }
+                    _ => {
+                        mouse_action_context.borrow_mut().button = "".to_string();
+                    }
+                }
                 app::handle_main(IntesEvent::MOUSE_DOWN).unwrap();
                 true
             }
@@ -70,17 +84,17 @@ impl MouseTestArea {
                 true
             }
             enums::Event::Enter => {
-                mouse_action_handler.borrow_mut().mouse_action = "".to_string();
                 app::handle_main(IntesEvent::MOUSE_IN).unwrap();
                 true
             }
             enums::Event::Leave => {
-                mouse_action_handler.borrow_mut().mouse_action = "".to_string();
                 app::handle_main(IntesEvent::MOUSE_OUT).unwrap();
                 true
             }
             enums::Event::Move => {
-                mouse_action_handler.borrow_mut().mouse_action = "Mouse Move".to_string();
+                let coords = app::event_coords();
+                mouse_action_context.borrow_mut().mouse_x = coords.0;
+                mouse_action_context.borrow_mut().mouse_y = coords.1;
                 app::handle_main(IntesEvent::MOUSE_MOVE).unwrap();
                 true
             }
@@ -96,8 +110,9 @@ impl MouseTestArea {
 // Extend widget::Widget via the member `inner` and add other initializers and constructors
 widget_extends!(MouseTestArea, widget::Widget, inner);
 
-fn draw_tabs() {
+fn draw_tabs() -> Vec<Box<dyn Accessible>> {
     let mut tab = Tabs::default_fill().center_of_parent();
+    let mut controls = Vec::new();
 
     let client_area = tab.client_area();
 
@@ -117,41 +132,30 @@ fn draw_tabs() {
     mouse_test_vpack.set_spacing(30);
 
     let mouse_handler = Rc::new(RefCell::new(MouseKeyboardActionContext {
-        mouse_action: String::new(),
-        mouse_button: String::new(),
         mouse_x: 0,
         mouse_y: 0,
+        button: "".to_string(),
     }));
 
-    add_controls_hpack(
-        &mouse_test_vpack,
-        30,
-        add_mouse_test_tab_first_line,
-        &mouse_test_vpack,
-        Rc::clone(&mouse_handler),
-    );
-    add_controls_hpack(
-        &mouse_test_vpack,
-        100,
-        add_mouse_test_tab_second_line,
-        &mouse_test_vpack,
-        Rc::clone(&mouse_handler),
-    );
-    add_controls_hpack(
-        &mouse_test_vpack,
-        30,
-        add_mouse_tab_mouse_action_row,
-        &mouse_test_vpack,
-        Rc::clone(&mouse_handler),
-    );
+    let part_definitions: [(
+        i32,
+        fn(&group::Pack, Rc<RefCell<MouseKeyboardActionContext>>) -> Vec<Box<dyn Accessible>>,
+    ); 4] = [
+        (30, add_mouse_test_tab_first_line),
+        (100, add_mouse_test_tab_second_line),
+        (30, add_mouse_tab_mouse_action_row),
+        (30, add_mouse_tab_mouse_info_row),
+    ];
 
-    add_controls_hpack(
-        &mouse_test_vpack,
-        30,
-        add_mouse_tab_mouse_info_row,
-        &mouse_test_vpack,
-        Rc::clone(&mouse_handler),
-    );
+    for (height, part_definition) in part_definitions {
+        controls.append(&mut add_controls_hpack(
+            &mouse_test_vpack,
+            height,
+            part_definition,
+            &mouse_test_vpack,
+            Rc::clone(&mouse_handler),
+        ));
+    }
     mouse_test_vpack.end();
     mouse_test_flex.end();
 
@@ -159,90 +163,154 @@ fn draw_tabs() {
     keyboard_test_flex.end();
     tab.end();
     tab.auto_layout();
+    return controls;
 }
 
 fn add_mouse_test_tab_first_line(
     _pack: &group::Pack,
     _event_handler: Rc<RefCell<MouseKeyboardActionContext>>,
-) {
+) -> Vec<Box<dyn Accessible>> {
     let mut button1 = button::Button::default()
+        .with_id("buttonA")
         .with_size(70, 0)
-        .with_label("Button1");
+        .with_label("Button A");
 
     button1.set_callback(|_| {
         app::handle_main(IntesEvent::BUTTON1_CLICK).unwrap();
     });
     let mut button2 = button::Button::default()
+        .with_id("buttonB")
         .with_size(70, 0)
-        .with_label("Button2");
+        .with_label("Button B");
 
     button2.set_callback(|_| {
         app::handle_main(IntesEvent::BUTTON2_CLICK).unwrap();
     });
+
+    return vec![Box::new(button1), Box::new(button2)];
 }
 
 fn add_mouse_tab_mouse_action_row(
     _pack: &group::Pack,
     _event_handler: Rc<RefCell<MouseKeyboardActionContext>>,
-) {
-    let _label = frame::Frame::default()
+) -> Vec<Box<dyn Accessible>> {
+    let label = frame::Frame::default()
         .with_size(90, 0)
         .with_label("Mouse action:");
     let mut input = input::IntInput::default().with_size(200, 0);
     input.handle(move |i, ev| match ev {
         IntesEvent::BUTTON1_CLICK => {
-            i.set_value("Button 1 clicked");
+            i.set_value("Button A clicked");
             true
         }
         IntesEvent::BUTTON2_CLICK => {
-            i.set_value("Button 2 clicked");
+            i.set_value("Button B clicked");
             true
+        }
+        IntesEvent::MOUSE_UP => {
+            i.set_value("Mouse up");
+            false
+        }
+        IntesEvent::MOUSE_DOWN => {
+            i.set_value("Mouse down");
+            false
+        }
+        IntesEvent::MOUSE_OUT => {
+            i.set_value("");
+            false
+        }
+        IntesEvent::MOUSE_MOVE => {
+            i.set_value("Mouse move");
+            false
         }
         _ => false,
     });
 
-    input.set_readonly(true)
+    input.set_readonly(true);
+    return vec![Box::new(label), Box::new(input)];
 }
 
 fn add_mouse_tab_mouse_info_row(
     _pack: &group::Pack,
-    _event_handler: Rc<RefCell<MouseKeyboardActionContext>>,
-) {
-    let _label = frame::Frame::default()
-        .with_size(90, 0)
+    event_context: Rc<RefCell<MouseKeyboardActionContext>>,
+) -> Vec<Box<dyn Accessible>> {
+    let label = frame::Frame::default()
+        .with_size(120, 0)
         .with_label("Mouse information:");
     let mut input = input::IntInput::default().with_size(200, 0);
+    input.handle(move |i, ev| match ev {
+        IntesEvent::MOUSE_UP => {
+            let mouse_info = format!(
+                "X: {}, y: {}, button: {}",
+                event_context.borrow().mouse_x,
+                event_context.borrow().mouse_y,
+                event_context.borrow().button
+            );
+            i.set_value(&mouse_info);
+            true
+        }
+        IntesEvent::MOUSE_DOWN => {
+            let mouse_info = format!(
+                "X: {}, y: {}, button: {}",
+                event_context.borrow().mouse_x,
+                event_context.borrow().mouse_y,
+                event_context.borrow().button
+            );
+            i.set_value(&mouse_info);
+            true
+        }
+        IntesEvent::MOUSE_MOVE => {
+            let mouse_info = format!(
+                "X: {}, y: {}",
+                event_context.borrow().mouse_x,
+                event_context.borrow().mouse_y
+            );
+            i.set_value(&mouse_info);
+            true
+        }
+        _ => {
+            i.set_value("");
+            false
+        }
+    });
     input.set_readonly(true);
-    input.set_value("aaaaa");
+    return vec![Box::new(label), Box::new(input)];
 }
 
 fn add_mouse_test_tab_second_line(
     pack: &group::Pack,
     event_handler: Rc<RefCell<MouseKeyboardActionContext>>,
-) {
-    let mut btn = MouseTestArea::new("Mouse Test Canvas", pack.width(), 100, event_handler);
+) -> Vec<Box<dyn Accessible>> {
+    let mut btn: MouseTestArea =
+        MouseTestArea::new("Mouse Test Canvas", pack.width(), 100, event_handler);
     // notice that set_color and set_callback are automatically implemented for us!
     btn.set_color(enums::Color::Cyan);
     btn.set_callback(|_| println!("Clicked"));
+
+    return vec![];
 }
 
 fn add_controls_hpack(
     grp1: &group::Pack,
     height: i32,
-    add_components: fn(&group::Pack, Rc<RefCell<MouseKeyboardActionContext>>),
+    add_components: fn(
+        &group::Pack,
+        Rc<RefCell<MouseKeyboardActionContext>>,
+    ) -> Vec<Box<dyn Accessible>>,
     pack: &group::Pack,
     event_handler: Rc<RefCell<MouseKeyboardActionContext>>,
-) {
+) -> Vec<Box<dyn Accessible>> {
     let mut hpack = group::Pack::default()
         .with_size(grp1.width(), height)
         .center_of(grp1);
 
     hpack.set_type(group::PackType::Horizontal);
-    hpack.set_spacing(30);
+    hpack.set_spacing(10);
 
-    add_components(pack, event_handler);
+    let components = add_components(pack, event_handler);
 
     hpack.end();
+    return components;
 }
 
 fn main() {
@@ -256,11 +324,13 @@ fn main() {
         .with_label("INTES: A GUI testing application")
         .center_screen();
 
-    draw_tabs();
+    let controls = draw_tabs();
 
     wind.make_resizable(true);
     wind.end();
     wind.show();
 
-    app.run().unwrap();
+    let ac = AccessibilityContext::new(wind, controls);
+
+    app.run_with_accessibility(ac).unwrap();
 }
